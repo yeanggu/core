8af4015937bcdb7108cf6346404ed046e3d5029b
yeeun <dpdms9621@naver.com>
2023-01-31 20:36:56 +0900

    [test] 스프링의 싱글톤방식 (Configuration)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,19 +15,43 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+    // 그럼 싱글톤이 깨지는 것인가? NO. 1번만 호출됨.
+
+    /* 예상 호출순서 (조금 다를수는 있음)
+        call AppConfig.memberService
+        call AppConfig.memberRepository
+        call AppConfig.memberRepository
+        call AppConfig.orderService
+        call AppConfig.memberRepository
+     */
+
+    /* 실제 호출
+        call AppConfig.memberService
+        call AppConfig.memberRepository
+        call AppConfig.orderService
+     */
+
     @Bean
     public MemberService memberService() {
+        //1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        //2번? 3번?
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+        //1번
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
