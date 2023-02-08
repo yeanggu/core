1d1ce0ff48058dfa7f0c1a1f736213fc3ca46852
yeeun <dpdms9621@naver.com>
2023-02-08 18:58:27 +0900

    [test] 싱글톤 빈과 함께 사용시 Provider로 문제 해결 - 2. Provider(스프링에 의존하지 않음)

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -33,17 +34,17 @@ public class SingletonWithPrototypeTest {
         assertThat(count1).isEqualTo(1);
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
 
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
